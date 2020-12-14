5b75ef0f219477363b0667b10ab6349c03c610a9
rg27 <gualvezreymar@gmail.com>
2020-12-14 14:07:06 +0800

    Add unclickable link in CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,12 @@ function Header() {
           </a>
           <div class="flex w-1/2 justify-end content-center">
             <Iframe 
-            src="https://free.timeanddate.com/countdown/i7l81l5x/n1238/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cacfff/cpcfff/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/matYear%20End%20Event%20Countdown/macfff/mptMerry%20Christmas!!!/mpcfff/iso2020-12-19T20:00:00" allowTransparency="true" frameborder="0" width="320" height="135"
+              src="https://free.timeanddate.com/countdown/i7l81l5x/n1238/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cacfff/cpcfff/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/matYear%20End%20Event%20Countdown/macfff/mptMerry%20Christmas!!!/mpcfff/iso2020-12-19T20:00:00" 
+              allowTransparency="true" 
+              frameborder="0" 
+              width="320" 
+              height="135"
+              className="unclickable"
             />
           </div>
         </div>
