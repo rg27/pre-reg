3ee8684666dd9e2431bbe1de85d492d39e6015c0
rg27 <gualvezreymar@gmail.com>
2020-12-16 09:39:36 +0800

    Modifying the timer

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,11 +17,11 @@ function Header() {
           </a>
           <div class="flex w-1/2 justify-end content-center">
             <Iframe 
-              src="https://free.timeanddate.com/countdown/i7l81l5x/n1238/cf100/cm0/cu4/ct0/cs0/ca0/cr0/ss0/cacfff/cpcfff/pct/tc66c/fs100/szw320/szh135/tac000/tpc000/matYear%20End%20Event%20Countdown/macfff/mptMerry%20Christmas!!!/mpcfff/iso2020-12-19T20:00:00" 
+              src="https://free.timeanddate.com/countdown/i7lbpwl0/n1238/cf100/cm0/cu4/ct0/cs1/ca0/cr0/ss0/cacfff/cpcfff/pct/tcfff/fs200/szw448/szh189/tacfff/tpcfff/matCountdown%20Timer/macfff/mptIt's%20Showtime/mpcfff/iso2020-12-19T20:00:00"
               allowTransparency="true" 
               frameborder="0" 
-              width="320" 
-              height="135"
+              width="450" 
+              height="200"
               className="unclickable"
             />
           </div>
